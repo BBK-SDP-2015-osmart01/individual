3915adab40b2215083a6b3deda30c3bee06994de
Oliver Smart <sdp@smartsci.uk>
2015-02-08 10:26:39 +0000

    remove unused import

--- src/sml/Translator.java
+++ src/sml/Translator.java
@@ -2,7 +2,6 @@ package sml;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
