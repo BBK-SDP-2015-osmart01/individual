a6c2804c82c2113702a54778be6fa008ddac8901
Oliver Smart <sdp@smartsci.uk>
2015-02-08 15:01:36 +0000

    Add javadoc comment for the class

--- src/sml/AddInstruction.java
+++ src/sml/AddInstruction.java
@@ -1,9 +1,15 @@
 package sml;
 
 /**
- * This class ....
+ * This class deals with SML "add" instruction
  * 
- * @author someone
+ * According to assignment sheet:
+ * 
+ * "L1 add r s1 s2" is an instruction to
+ * "Add the contents of register s1 and s2 and store the result in register r"
+ * 
+ * @author SDP course work code modified by Oliver Smart
+ *         <osmart01@dcs.bbk.ac.uk>
  */
 
 public class AddInstruction extends Instruction {
