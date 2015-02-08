6f0d9de2c471af4a2ac9a361c0431aea9927644e
Oliver Smart <sdp@smartsci.uk>
2015-02-08 20:25:00 +0000

    Add new intermediate abstract class ... ArithmeticInstruction to avoid code duplication in the add, sub, mul and div operation code.
    Test results look OK:
    
       $ java -cp ./bin/ sml/Machine add.sml
    	Here is the program; it has 4 instructions.
    	f0: lin register 20 value is 9
    	f1: lin register 21 value is -7
    	f2: add contents of reg 20 and reg 21 storing result in reg 10
    	f3: out print register 10 contents to Java console
    
    	Beginning program execution.
    	2
    	Ending program execution.
    	Values of registers at program termination:
    	Registers(registers=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, -7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]).
    
       $ java -cp ./bin/ sml/Machine mul.sml
    	Here is the program; it has 4 instructions.
    	f0: lin register 20 value is 9
    	f1: lin register 21 value is -7
    	f2: mul contents of reg 20 and reg 21 storing result in reg 10
    	f3: out print register 10 contents to Java console
    
    	Beginning program execution.
    	-63
    	Ending program execution.
    	Values of registers at program termination:
    	Registers(registers=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -63, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, -7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]).
    
       $ java -cp ./bin/ sml/Machine code.sml
    	WARNING have unrecognized instruction='bnz' on line 'f5  bnz 20 f3'
    	Here is the program; it has 6 instructions.
    	f0: lin register 20 value is 6
    	f1: lin register 21 value is 1
    	f2: lin register 22 value is 1
    	f3: mul contents of reg 21 and reg 20 storing result in reg 21
    	f4: sub contents of reg 20 and reg 22 storing result in reg 20
    	f6: out print register 21 contents to Java console
    
    	Beginning program execution.
    	6
    	Ending program execution.
    	Values of registers at program termination:
    	Registers(registers=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 6, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]).

---
+++ src/sml/ArithmeticInstruction.java
@@ -0,0 +1,53 @@
+package sml;
+
+/**
+ * Attempt to avoid large code duplication in AddInstruction,
+ * SubInstruction ...
+ * 
+ * @author SDP course work code modified by Oliver Smart
+ *         <osmart01@dcs.bbk.ac.uk>
+ */
+
+public abstract class ArithmeticInstruction extends Instruction {
+
+	protected int result;
+	protected int op1;
+	private int op2;
+
+	public ArithmeticInstruction(String label, String opcode, int result,
+			int op1, int op2) {
+		super(label, opcode);
+		if (opcode.equals("add") || opcode.equals("sub")
+				|| opcode.equals("mul") || opcode.equals("div")) {
+			this.result = result;
+			this.op1 = op1;
+			this.op2 = op2;
+		} else {
+			throw new RuntimeException("unrecognized opcode "+ opcode);
+		}
+	}
+
+	@Override
+	public void execute(Machine m) {
+		int value1 = m.getRegisters().getRegister(op1);
+		int value2 = m.getRegisters().getRegister(op2);
+		int valueResult = Integer.MAX_VALUE;
+		if (opcode.equals("add")) {
+			valueResult = value1 + value2;
+		} else if (opcode.equals("sub")) {
+			valueResult = value1 - value2;
+		} else if (opcode.equals("mul")) {
+			valueResult = value1 * value2;
+		} else if (opcode.equals("mul")) {
+			valueResult = value1 / value2;
+		}
+		m.getRegisters().setRegister(result, valueResult);
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + " contents of reg " + op1 + " and reg " + op2
+				+ " storing result in reg " + result;
+	}
+
+}
