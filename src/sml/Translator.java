5191b30c87737cbfc1fd3e9d481b0d10a88e95ad
Oliver Smart <sdp@smartsci.uk>
2015-02-15 18:39:06 +0000

    remove unused import

--- src/sml/Translator.java
+++ src/sml/Translator.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
